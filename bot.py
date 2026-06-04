7445a90a51988124d2d6c2660cf76d46e0d48240
Maksim Kosaretskiy <ma.ko1995@mail.ru>
2026-06-04 14:42:34 +0300

    'Update bot.py'

--- bot.py
+++ bot.py
@@ -46,7 +46,13 @@ async def main():
     await dp.start_polling(bot)
 
 async def run_bot():
-    await main()
+    while True:
+        try:
+            await main()
+        except Exception as e:
+            logger.error(f'Ошибка запуска бота: {e}')
+            logger.info('Повторное подключение через 30 секунд...')
+            await asyncio.sleep(30)
 
 
 if __name__ == '__main__':
